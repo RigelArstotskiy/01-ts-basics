8dfbd82e0311f29c7e402600f1805b67123526f0
Mykyta <nik.from.varga@gmail.com>
2025-09-10 23:04:48 +0300

    all-tasks-done

---
+++ src/task-6.ts
@@ -0,0 +1,12 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+getFirstElement([1, 2, 3]); // 1
+getFirstElement(["a", "b", "c"]); // "a"
+getFirstElement([true, false, true]); // true
+
+// testing how code working
+console.log(getFirstElement([1, 2, 3]));
+console.log(getFirstElement(["a", "b", "c"]));
+console.log(getFirstElement([true, false, true]));
